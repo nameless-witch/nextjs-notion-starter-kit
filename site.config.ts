2dc170fa0c3229cb56144f8369186f0b5895dcaa
nameless-witch <121109774+nameless-witch@users.noreply.github.com>
2023-03-21 12:13:13 -0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: '',
   github: '',
-  linkedin: 'Siying Wang',
+  linkedin: 'http://www.linkedin.com/in/Siying20Wang',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
